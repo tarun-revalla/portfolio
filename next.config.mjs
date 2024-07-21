799b059aa896cb5ac34088ae6666226cfcfdb98c
tarunrevalla <tarunrevalla@gmail.com>
2024-07-21 12:13:17 +0000

    pwa impellementation 1

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,12 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-};
+// next.config.mjs
+import withPWA from 'next-pwa';
 
-export default nextConfig;
+const nextConfig = withPWA({
+  dest: 'public',
+  disable: process.env.NODE_ENV === 'development',
+});
+
+export default nextConfig({
+  reactStrictMode: true,
+  ...nextConfig,
+});
--- pages/_document.jsx
+++ pages/_document.jsx
@@ -3,7 +3,26 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/icons/apple-touch-icon.png"
+        />
+        <link
+          rel="icon"
+          href="/favicon.ico"
+        />
+      
+        <link rel="manifest" href="/manifest.json" />
+        <link
+          rel="mask-icon"
+          href="/icons/safari-pinned-tab.svg"
+          color="#5bbad5"
+        />
+        <meta name="msapplication-TileColor" content="#da532c" />
+        <meta name="theme-color" content="#000000" />
+      </Head>
       <body>
         <Main />
         <NextScript />
