1e6a2c35f8d03faa4737cba2f5418bb35a67d251
tarunrevalla <tarunrevalla@gmail.com>
2024-07-21 12:26:13 +0000

    pwa 2

--- pages/_document.jsx
+++ pages/_document.jsx
@@ -7,17 +7,13 @@ export default function Document() {
         <link
           rel="apple-touch-icon"
           sizes="180x180"
-          href="/icons/apple-touch-icon.png"
+          href="/apple-touch-icon.png"
         />
-        <link
-          rel="icon"
-          href="/favicon.ico"
-        />
-      
-        <link rel="manifest" href="/manifest.json" />
+        <link rel="icon" href="/favicon.ico" />
+        <link rel="manifest" href="/icons/site.webmanifest" />
         <link
           rel="mask-icon"
-          href="/icons/safari-pinned-tab.svg"
+          href="/safari-pinned-tab.svg"
           color="#5bbad5"
         />
         <meta name="msapplication-TileColor" content="#da532c" />
